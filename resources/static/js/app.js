82969dd634fea6e23b2d4d3f516166510a382f4a
Spud Hsu <supersheepxjy@gmail.com>
2012-04-26 14:08:46 +0800

    add widget manager

--- resources/static/js/app.js
+++ resources/static/js/app.js
@@ -11,15 +11,14 @@ YUI(YUI_CONFIG).use('desktopManager','monitorFactory','widgetManager','dd-drag',
 		toolbox = wrap.one('.toolbox'),
 		toggle = toolbox.one('.toggle'),
 		manage = toolbox.one('.manage'),
-		items = toolbox.all('.item'),
 		toggled = false,
 		mousefield,
 		widgetManager,
-		desktop;
+		desktopManager;
 	
 	function render(){
 		wrap.setStyle('height',wrap.get('winHeight') - ( hd ? hd.get('clientHeight') : 0) );
-		desktop.render();
+		desktopManager.render();
 	}
 	
 	function bindEvents(){
@@ -39,139 +38,99 @@ YUI(YUI_CONFIG).use('desktopManager','monitorFactory','widgetManager','dd-drag',
 		 *
 		**/
 		manage.on('click',function(){
-			//Y.WidgetManagePannel.show();
 			if(toggled){
 				widgetManager.show();
 			}
 		});
 		
-		/**
-		 * Item Drag to The Board
-		 *
-		**/
-		Y.use('')	
-			// init drag instances
-			items.each(function(e,i){
-				var drag= new Y.DD.Drag({
-					node: e
-				}).plug(Y.Plugin.DDProxy,{
-					moveOnEnd: false
-				});
-				
-				/*
-				drag.plug(Y.Plugin.DDConstrained,{
-					constrain2node: toolbox
-				});
-				*/
-				
-				drag.on('drag:start',function(){
-					var p = this.get('dragNode'),
-						n = this.get('node');
-
-						p.setStyle('opacity', .25);
-						if (!this._playerStart) {
-							this._playerStart = this.nodeXY;
-						}
-						p.set('innerHTML', n.get('innerHTML'));
-						p.setStyles({
-							backgroundColor: n.getStyle('backgroundColor'),
-							color: n.getStyle('color'),
-							border:'none',
-							opacity: .65
-						});
-						p.addClass('item');						
-				});
-				
-				drag.on('drag:end', function(e) {
-					var n = this.get('node');
-					n.setStyle('opacity', '1');
-				});
-				
-				drag.on('drag:drophit', function(e) {
-					var xy = e.target.mouseXY,
-						node = this.get('node');
-						
-					// getProperties from the node
-					// and init a pannel with them and axis
-					
-					// infact, here I should new a "Pannel" with configs and ajaxUrl
-					// what a Pannel do: 
-					// a Pannel has its default configuration
-					
-					var type = node.getAttribute('data-type'),
-						xkey= node.getAttribute('data-xkey'),
-						ykeys = node.getAttribute('data-ykeys'),
-						names = node.getAttribute('data-names'),
-						title = node.getAttribute('data-title');
-						
-					// add a monitor to a desktop
-					Y.MonitorFactory.produce(type,xy,desktop.getCurrent(),{
-						setting:{
-							"xkey":xkey,
-							"ykeys":ykeys,
-							"names":names						
-						}
-					});
-					
-				});
-						
-			// init drop instances
-			var drop = new Y.DD.Drop({
-				node: main
-			});
-			
-			// mousemove events to show/hide controls
-			Y.on('mousemove',function(e){
-				var winHeight = wrap.get('winHeight'),
-					winWidth = wrap.get('winWidth'),
-					margin = 120,
-					x = e.clientX,
-					y = e.clientY;
-					
-				if((x>margin && x<winWidth-margin)&&(y>margin)){
-					if(mousefield!="inside"){
-						desktop && desktop.hideControls();
-					}
-					mousefield = "inside";	
-				}else{
-					if(mousefield!="edge"){
-						desktop && desktop.showControls();	
-					}
-					mousefield = "edge";		
-				}
-			});
-			
-			
-		});
 		
+		new Y.DD.Drop({
+			node: main
+		});
+
+	
+		// mousemove events to show/hide controls
+		Y.on('mousemove',function(e){
+			var winHeight = wrap.get('winHeight'),
+				winWidth = wrap.get('winWidth'),
+				margin = 120,
+				x = e.clientX,
+				y = e.clientY;
+				
+			if((x>margin && x<winWidth-margin)&&(y>margin)){
+				if(mousefield!="inside"){
+					desktopManager && desktopManager.hideControls();
+				}
+				mousefield = "inside";	
+			}else{
+				if(mousefield!="edge"){
+					desktopManager && desktopManager.showControls();	
+				}
+				mousefield = "edge";		
+			}
+		});
 		
 	}
 	
 	
 	Y.on('keydown',function(e){
 		switch(e.keyCode){
-			case 39:desktop.right();break; //right
-			case 37:desktop.left();break; //left
+			case 39:desktopManager.right();break; //right
+			case 37:desktopManager.left();break; //left
 			case 38:;break; //up
 			case 40:;break; //down
 		}
 	});
 	
+	
+	function init(desk,widgets,allwidgets){
+		if(desk && widgets){
+			desk = 	JSON.parse(desk);
+			widgets = JSON.parse(widgets);
+			
+			desktopManager = new Y.DesktopManager(main,desk);
+			widgetManager = new Y.WidgetManager(allwidgets,widgets,desktopManager);
+			
+			render();
+			bindEvents();
+		}
+	}
+	
 	/**
 	 * Deal Global Events
 	**/
 	Y.on('domready',function(){
-		var desktopData = JSON.parse(localStorage.getItem("desktops")),
-			myWidgets = JSON.parse(localStorage.getItem("widgets")),
-			widgetsData = widgets;// global
+		var ls_desktopData = localStorage.getItem("desktops"),
+			ls_widgets = localStorage.getItem("widgets");
 		
+		function success(id,o,type){
+			if(type == "desk"){
+				ls_desktopData = o.responseText;
+			}else if(type == "widgets"){
+				ls_widgets = o.responseText;
+			}
+			init(ls_desktopData,ls_widgets,widgetsData);
+		}	
+		
+			
+		init(ls_desktopData,ls_widgets,widgetsData);	
+		
+		if(!ls_desktopData){
+			YUI().use('io', function (Y) {
+			    Y.on('io:success', success,Y,'desk');
+   				Y.io(APP_CONFIG['deskUrl']);
+			});
+		}
+		
+		if(!ls_widgets){
+			YUI().use('io', function (Y) {
+			    Y.on('io:success', success,Y,'widgets');
+   				Y.io(APP_CONFIG['widgetUrl']);
+			});
+		}
 		// console.log('initdata',localStorage.getItem("desktops"),desktopData);
 		
 		
-		widgetManager = new Y.WidgetManager(widgetsData,myWidgets);
-		desktop = new Y.DesktopManager(main,desktopData);
-		render();
-		bindEvents();
 	});
 	
 	Y.on('windowresize',render);
--- resources/static/js/app/widgetManager.js
+++ resources/static/js/app/widgetManager.js
@@ -1,49 +1,111 @@
 YUI.add('widgetManager',function(Y){
+
 	function dom(tag,cls){
+	
 		var node = Y.Node.create('<'+tag+' />');
 		cls && node.addClass(cls);
 		return node;
-	}
-	function div(cls){
-		return dom('div',cls);
+		
 	}
 	
-	function WidgetManager(widgets,me){
-		var wrap = div('widgets'),
+	function div(cls){
+	
+		return dom('div',cls);
+		
+	}
+	
+	
+	function initDrag(li,o,desktops){
+		var drag= new Y.DD.Drag({
+			node: li
+		}).plug(Y.Plugin.DDProxy,{
+			moveOnEnd: false
+		});
+		
+		drag.on('drag:start',function(){
+			var p = this.get('dragNode'),
+				n = this.get('node');
+
+			p.setStyle('opacity', .25);
+			if (!this._playerStart) {
+				this._playerStart = this.nodeXY;
+			}
+			p.set('innerHTML', n.get('innerHTML'));
+			p.setStyles({
+				backgroundColor: n.getStyle('backgroundColor'),
+				color: n.getStyle('color'),
+				border:'none',
+				opacity: .65
+			});
+			p.addClass('item');						
+		});
+		
+		drag.on('drag:end', function(e) {
+			var n = this.get('node');
+			n.setStyle('opacity', '1');
+		});
+		
+		drag.on('drag:drophit', function(e) {
+			var xy = e.target.mouseXY,
+				node = this.get('node');
+				
+				
+			// add a monitor to a desktop
+			Y.MonitorFactory.produce(o.title,xy,desktops.getCurrent(),{
+				setting:o.setting
+			});
+			
+		});
+	}
+
+
+	
+	function WidgetManager(widgets,my_widgets,desktops){
+		var self = this,
+			wrap = div('widgets'),
 			ul = dom('ul','list'),
 			elem = this.elem = div('widget-manager'),
 			close = this.close  = div('close');
 		
-		me = me || [{name:"line"}];
-		
+		// my_widgets = my_widgets || [{name:"line"}];
+		console.log(my_widgets);
 		close.on('click',Y.bind(this.hide,this));
 		
 		elem.append(close);
 		elem.append(wrap);
 		wrap.append(ul);
+		
+		self.my_widgets = my_widgets;
+		self.tools = Y.Node.one('.tools');
+		self.desktops = desktops;
 		widgets.forEach(function(e,i){
 			var li = dom('li','clear ' + (i%2?'order':'even')),
 				checkbox = dom('input','checkbox').set('type','checkbox'),
 				img = dom('img','icon').set('src',e.icon),
-				name = dom('span','name').set('innerHTML',e.name);
+				checked = false,
+				title = dom('span','title').set('innerHTML',e.title);
 			
-			name = e.name;
-			if(me.some(function(m){
-				return m.name == name;
+			console.log(JSON.stringify(e));
+			
+			if(my_widgets.some(function(m){
+				return m.title == e.title;
 			})){
-				
-				li.addClass('active');
-				checkbox.set('checked','checked');
-				
+				checked = true;
+				self.addWidget(e,li,checkbox);
 			}
 			
 			checkbox.on('click',function(){
-				li.toggleClass('active');
+				checked = !checked;
+				if(checked){
+					self.addWidget(e,li,checkbox);
+				}else{
+					self.removeWidget(e,li,checkbox);
+				}
 			});
 				
 			li.append(checkbox);
 			li.append(img);
-			li.append(name);
+			li.append(title);
 			ul.append(li);
 		});
 		
@@ -51,12 +113,72 @@ YUI.add('widgetManager',function(Y){
 			node: elem
 		});		
 		elem.appendTo('body');
-		this.hide();
+		self.hide();
 		
 	}
-	
+		
 	WidgetManager.prototype = {
 		constructor : WidgetManager,
+		addWidget:function(e,li,checkbox){
+			var self = this,
+				witem = dom(li,'item'),
+				img = dom('img'),
+				p = dom('p','title');
+			
+			li.addClass('active');
+				
+			img.set('src',e.icon);
+			img.set('alt',e.title);
+				
+			p.set('innerHTML',e.title);
+			
+			witem.setAttribute('data-title',e.title);
+			witem.append(img);
+			witem.append(p);
+			
+			initDrag(witem,e,self.desktops);
+			
+			checkbox.set('checked','checked');
+			
+			self.tools.append(witem);
+			self.my_widgets.push(e);
+			self.sync();
+		},
+		removeWidget:function(e,li,checkbox){
+			var self = this,
+				my_widgets = self.my_widgets;
+			
+			
+			li.removeClass('active');
+			
+			Y.Node.all('.tools .item').each(function(el,i){
+				if(el.getAttribute('data-title') == e.title){
+					el.remove(true);
+				}
+			});
+			
+			my_widgets.forEach(function(el,i){
+				if(e.title == el.title){
+					my_widgets = my_widgets.slice(0,i).concat(my_widgets.slice(i+1,length));	
+					self.my_widgets = my_widgets;
+				}
+			});
+			
+			self.sync();
+		},
+		sync:function(){
+			var data = JSON.stringify(this.my_widgets);
+			localStorage.setItem('widgets',data);
+			YUI().use('io',function(Y){
+				Y.on('io:success', function(){
+					console.log('widget saved');
+				});
+   				Y.io(APP_CONFIG['widgetUrl'],{
+					method: 'POST',
+					data: 'data='+data
+   				});
+			});
+		},
 		show : function(){
 			console.log('BOOM!!!');
 			this.elem.setStyle('display','block');
