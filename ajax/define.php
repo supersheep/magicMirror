dbba659d81dd05faefa4a9a862be0ee6f2dda29d
Spud Hsu <supersheepxjy@gmail.com>
2012-05-10 18:21:23 +0800

    add fake data

---
+++ ajax/define.php
@@ -0,0 +1,8 @@
+<?php
+	/*
+	var_dump($_FILES);
+	var_dump($_POST);
+	*/
+	echo '{"config":{"title":"Linmme!","names":"line","xkey":"time","ykeys":"cn,cn"},"id":"giuyiuf","icon":"/resources/static/img/1.png"}';
+	
+	
---
+++ ajax/widget.php
@@ -0,0 +1,1 @@
+{"data":"[1]","mirrorManager":{},"type":"widget","userId":"-13575"}
